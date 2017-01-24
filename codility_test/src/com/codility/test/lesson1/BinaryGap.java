7f0be3d51e74490b5e942134c5dd1682941edcb0
Darlan Araújo dos Santos <darlan.ads@gmail.com>
2017-01-24 15:20:36 -0300

    Added source the solutions codility.

---
+++ codility_test/src/com/codility/test/lesson1/BinaryGap.java
@@ -0,0 +1,10 @@
+package com.codility.test.lesson1;
+
+public class BinaryGap {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.solution(1041));
+	}
+
+}
